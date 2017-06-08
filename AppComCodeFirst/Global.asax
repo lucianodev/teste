ab0ead2d29795471840b86398a77f4dd3e1f5f8c
Luciano Menezes Gonçalves <lucianodev@gmail.com>
2017-06-08 00:44:28 -0300

    teste de commit

---
+++ AppComCodeFirst/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AppComCodeFirst.MvcApplication" Language="C#" %>
---
+++ AppComCodeFirst/Models/Categorias.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace AppComCodeFirst.Models
+{
+    public class Categorias
+    {
+        [Key]
+        public int CategoriaID { get; set; }
+
+        public String Categoria { get; set; }
+
+        public String Descricao { get; set; }
+
+        public virtual ICollection<Posts> Posts { get; set; }
+
+    }
+
+    public class Posts
+    {
+        [Key]
+        public long PostID { get; set; }
+
+        public String TituloPost { get; set; }
+
+        public String ResumoPost { get; set; }
+
+        public String ConteudoPost { get; set; }
+
+        public DateTime DataPostagem { get; set; }
+
+        public int CategoriaID { get; set; }
+
+        [ForeignKey("CategoriaID")]
+        public virtual Categorias Categorias { get; set; }
+
+    }
+
+   
+}
+        
